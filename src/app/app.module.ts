59f201183790d0127ad8e7bc09238c62ac4ac764
Neha <shukla.ne@husky.neu.edu>
2018-06-12 22:56:41 -0400

    changes in search panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -14,7 +15,8 @@ import { HomeSearchPanelComponent } from './home-search-panel/home-search-panel.
     HomeSearchPanelComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
