b07adf94f9086119701e2404f28e3b59a0f37fcf
Neha <shukla.ne@husky.neu.edu>
2018-06-13 17:38:38 -0400

    changes for build

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+
+
+
+
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -5,12 +9,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/webdev-project-client'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join('./dist/webdev-project-client/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8000);
