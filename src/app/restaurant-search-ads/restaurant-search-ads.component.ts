94097b7093bcd8f016d1bcbb3e6867a6e69e2686
Neha <nehagundecha95@gmail.com>
2018-06-14 16:54:21 -0400

    Added restaurant searh filter column

---
+++ src/app/restaurant-search-ads/restaurant-search-ads.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant-search-ads',
+  templateUrl: './restaurant-search-ads.component.html',
+  styleUrls: ['./restaurant-search-ads.component.css']
+})
+export class RestaurantSearchAdsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/restaurant-search-list/restaurant-search-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant-search-list',
+  templateUrl: './restaurant-search-list.component.html',
+  styleUrls: ['./restaurant-search-list.component.css']
+})
+export class RestaurantSearchListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
