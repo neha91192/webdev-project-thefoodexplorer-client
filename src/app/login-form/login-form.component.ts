5e1c20a03ee354db620312a08c68225abf91f98d
Neha <shukla.ne@husky.neu.edu>
2018-06-19 21:46:44 -0400

    added login and login form

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -19,4 +19,22 @@ export class LoginFormComponent implements OnInit {
     this.loginFailureMessage = '';
   }
 
+  login(c) {
+    const user = {
+      username: this.username,
+      password: this.password
+    };
+    console.log(user);
+    // this.service.login(user).then((response) => {
+    //   if (response === 'login failed') {
+    //     // alert('login failed');
+    //     this.loginFailureMessage = 'Incorrect Username or Password';
+    //   } else {
+    //     c('Cross click');
+    //     this.router.navigate(['profile']);
+    //   }
+    //
+    // });
+  }
+
 }
