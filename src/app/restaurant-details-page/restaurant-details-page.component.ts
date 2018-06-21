5516a683469d87f7ea2307e5eb6b36b881d8c7b4
Neha <shukla.ne@husky.neu.edu>
2018-06-21 03:47:07 -0400

    updated restaurant details page

--- src/app/restaurant-details-page/restaurant-details-page.component.ts
+++ src/app/restaurant-details-page/restaurant-details-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ZomatoApiServiceClient} from '../api-services/zomato-api-service-client';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-restaurant-details-page',
@@ -6,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./restaurant-details-page.component.css']
 })
 export class RestaurantDetailsPageComponent implements OnInit {
+  restaurantId;
+  restaurant;
 
-  constructor() { }
+  constructor(private zomatoService: ZomatoApiServiceClient, private route: ActivatedRoute) {
+    this.route.params.subscribe(params => this.setParams(params));
 
+  }
+
+  setParams(params) {
+    this.restaurantId = params['restaurantId'];
+    this.fetchRestaurant(this.restaurantId);
+  }
+
+  fetchRestaurant(restaurantId) {
+    this.restaurantId = restaurantId;
+    this.zomatoService.fetchRestaurant(restaurantId)
+      .then(restaurant => this.restaurant = restaurant);
+  }
   ngOnInit() {
   }
 
